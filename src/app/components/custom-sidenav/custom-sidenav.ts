1c992ffd070c1948484ada798d9283247f3d0552
Paulo Bispo <63209799+Paulo555Bispo@users.noreply.github.com>
2025-10-05 20:47:24 -0300

    Criação de lista de menus, ícones e rotas, usando sidenav.

--- src/app/components/custom-sidenav/custom-sidenav.ts
+++ src/app/components/custom-sidenav/custom-sidenav.ts
@@ -1,22 +1,23 @@
 import { MatIconModule } from '@angular/material/icon';
 import { Component, computed, Input, signal } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 export type MenuItem = {
   icon: string;
   label: string;
   route: string;
+  activeIcon?: string;
 }
-
 
 @Component({
   selector: 'app-custom-sidenav',
-  imports: [MatListModule, MatIconModule],
+  imports: [MatListModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './custom-sidenav.html',
   styleUrl: './custom-sidenav.scss'
 })
 export class CustomSidenav {
-
 
   sideNavCollapsed = signal(false);
 
@@ -27,21 +28,25 @@ export class CustomSidenav {
   menuItems = signal<MenuItem[]>([
     {
       icon: 'dashboard',
+      activeIcon: 'dashboard',
       label: 'Painel',
       route: 'dasboard',
     },
     {
       icon: 'video_library',
+      activeIcon: 'video_library',
       label: 'Conteúdo',
       route: 'content',
     },
     {
       icon: 'assessment',
+      activeIcon: 'assessment',
       label: 'Análises',
       route: 'analitics',
     },
     {
       icon: 'comments',
+      activeIcon: 'comments',
       label: 'Comentários',
       route: 'comments',
     }

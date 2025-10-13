0eb7aff8e13aa60627b84751aacd263b7c2ab6c6
Paulo Bispo <63209799+Paulo555Bispo@users.noreply.github.com>
2025-10-12 21:03:17 -0300

    Correção de cores e inclução de botões e painel no Dashboard.

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatCardModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
